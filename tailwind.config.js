3b8a1c8dd3bb4b3f6436f37b085960bbef41001b
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-10-12 14:17:57 +0800

    fix tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/*.{Vue,html,js}"],
+  content: ["./index.html", "./src/**/*.{vue,html,js}"],
   theme: {
     backgroundColor: {
       transparent: "transparent",
